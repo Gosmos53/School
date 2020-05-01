b79b95935748896787a4c28325f301ee3b04fb2e
Gosmos53 <64580852+Gosmos53@users.noreply.github.com>
2020-05-01 11:15:26 +0200

    hello

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # School
 mina projekt till skolan
+
+
+
+dessa projekt är öppen källkod 
+© Simon Fräjdin
---
+++ sten-sax-påse.py
@@ -0,0 +1,8 @@
+from random import randint
+
+val = ["sten","sax","påse"]
+
+dator = val[randint(0,2)]
+
+print("välkommen till mitt spel\n")
+spelare = input("ditt val:")
