1aee2b5f94e9875e358b7cd353af9cfe44bf0fcb
Gosmos53 <64580852+Gosmos53@users.noreply.github.com>
2020-05-01 11:56:26 +0200

    färdigt

--- sten-sax-påse.py
+++ sten-sax-påse.py
@@ -5,4 +5,25 @@ val = ["sten","sax","påse"]
 dator = val[randint(0,2)]
 
 print("välkommen till mitt spel\n")
-spelare = input("ditt val:")
+print("du kan välja mellan: sten, sax eller påse\n")
+print("lycka till!\n")
+
+spelare = input("ditt val:").lower()
+print("datorn valde:" + dator)
+
+
+
+if spelare == dator:
+    print("ovagjort") 
+elif spelare == "sax" and dator == "sten":
+    print("datorn vinner")
+elif spelare == "påse" and dator == "sax":
+    print("datron vinner")
+elif spelare == "sten" and dator == "påse":
+    print("datorn vinner")
+elif spelare == "sten" and dator == "sax":
+    print("spelare vinner")
+elif spelare == "sax" and dator == "påse":
+    print("spelaren vinner")
+elif spelare == "påse" and dator == "sten":
+    print("spelare vinner") 
